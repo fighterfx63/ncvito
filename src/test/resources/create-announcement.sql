4bdcdb9b348243bebdae4f3d4b5790aea1506ac8
fighterfx63 <valeev.rr63@gmail.com>
2019-07-24 17:45:27 +0400

    Authorization and authentication implementation and test

---
+++ src/test/resources/create-announcement.sql
@@ -0,0 +1,6 @@
+insert into apartment(id, address, floor, room_count, square) values
+(1,'Pobeda',1,1,34);
+
+
+insert into announcement(id, creation_date, description, is_sale, price, apartment_id, user_id) values
+(1,current_date ,'announcement_1',true,150,1,1);
