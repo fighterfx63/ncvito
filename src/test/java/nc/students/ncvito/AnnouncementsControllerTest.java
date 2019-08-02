68a8b2d5294a27aea96f9bf7d82b1ab9a68a097f
fighterfx63 <valeev.rr63@gmail.com>
2019-08-03 01:16:17 +0400

    Fix test problems

--- src/test/java/nc/students/ncvito/AnnouncementsControllerTest.java
+++ src/test/java/nc/students/ncvito/AnnouncementsControllerTest.java
@@ -5,94 +5,118 @@ import nc.students.ncvito.entity.Apartment;
 import nc.students.ncvito.entity.Role;
 import nc.students.ncvito.entity.User;
 import nc.students.ncvito.service.AnnouncementService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
 
-import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.core.Is.is;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource("/application-test.properties")
 
-@WithUserDetails("admin")
-@Sql(value = {"/create..sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AnnouncementsControllerTest {
 
-
-    @Autowired
-    private AnnouncementService announcementService;
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    public void findAll() throws Exception {
+    @Autowired
+    AnnouncementService announcementService;
 
-        this.mockMvc.perform(get("/announcements"))
-                .andDo(print())
-                .andExpect(authenticated())
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("announcement_1")));
-
-    }
-
-    @Test
-    public void getByID() throws Exception {
-        this.mockMvc.perform(get("/announcements/1"))
-                .andDo(print())
-                .andExpect(authenticated())
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("announcement_1")));
-    }
-
-    @Test
-    public void add() throws Exception {
+    @Before
+    public void createObjects() {
 
         User user = new User();
-
-        user.setLogin("qwe");
-        user.setPassword("qwe");
+        user.setLogin("admin");
+        user.setPassword("admin");
+        user.setEmail("email");
+        user.setFirstName("Petya");
+        user.setLastName("Petrov");
+        user.setPhone("123");
         user.setRole(Collections.singleton(Role.USER));
 
         Apartment apartment = new Apartment();
-
         apartment.setAddress("qwe");
         apartment.setFloor(1);
         apartment.setRoomCount(1);
         apartment.setSquare(1);
 
         Announcement announcement = new Announcement();
-
         announcement.setAuthor(user);
         announcement.setApartment(apartment);
         announcement.setSale(true);
         announcement.setPrice(150);
-        announcement.setDescription("test");
-
+        announcement.setDescription("announcement_1");
         announcementService.create(announcement);
+    }
+
+    @WithMockUser(username = "admin")
+    @Test
+    public void findAll() throws Exception {
+        this.mockMvc.perform(get("/announcements"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.content[0].description", is("announcement_1")));
+    }
+
+    @WithMockUser(username = "admin")
+    @Test
+    public void getByID() throws Exception {
+        this.mockMvc.perform(get("/announcements/1"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.description", is("announcement_1")));
+    }
+
+    @WithMockUser(username = "admin")
+    @Test
+    public void add() throws Exception {
+
+        User user1 = new User();
+        user1.setLogin("qwe");
+        user1.setPassword("qwe");
+        user1.setRole(Collections.singleton(Role.USER));
+
+        Apartment apartment1 = new Apartment();
+        apartment1.setAddress("qwe");
+        apartment1.setFloor(1);
+        apartment1.setRoomCount(1);
+        apartment1.setSquare(1);
+
+        Announcement announcement1 = new Announcement();
+        announcement1.setAuthor(user1);
+        announcement1.setApartment(apartment1);
+        announcement1.setSale(true);
+        announcement1.setPrice(150);
+        announcement1.setDescription("test");
+
+        announcementService.create(announcement1);
 
         this.mockMvc.perform(get("/announcements"))
                 .andDo(print())
                 .andExpect(authenticated())
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString(user.getLogin())));
+                .andExpect(jsonPath("$.content[0].author.login", is("admin")));
     }
 }
 
--- src/test/java/nc/students/ncvito/LoginTest.java
+++ src/test/java/nc/students/ncvito/LoginTest.java
@@ -1,17 +1,32 @@
 package nc.students.ncvito;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nc.students.ncvito.entity.Announcement;
+import nc.students.ncvito.entity.Apartment;
+import nc.students.ncvito.entity.Role;
+import nc.students.ncvito.entity.User;
+import nc.students.ncvito.service.AnnouncementService;
+import nc.students.ncvito.service.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.awt.*;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -22,7 +37,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @TestPropertySource("/application-test.properties")
-@Sql(value = {"/create..sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,38 +45,82 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class LoginTest {
 
     @Autowired
+    UserService userService;
+    @Autowired
+    AnnouncementService announcementService;
+    @Autowired
     private MockMvc mockMvc;
 
+    @Before
+
+    public void createObjects() {
+        User user = new User();
+
+        user.setLogin("admin");
+        user.setPassword("admin");
+        user.setEmail("email");
+        user.setFirstName("Petya");
+        user.setLastName("Petrov");
+        user.setPhone("123");
+        user.setRole(Collections.singleton(Role.USER));
+
+        Apartment apartment = new Apartment();
+
+        apartment.setAddress("qwe");
+        apartment.setFloor(1);
+        apartment.setRoomCount(1);
+        apartment.setSquare(1);
+
+        Announcement announcement = new Announcement();
+
+        announcement.setAuthor(user);
+        announcement.setApartment(apartment);
+        announcement.setSale(true);
+        announcement.setPrice(150);
+        announcement.setDescription("announcement_1");
+        announcementService.create(announcement);
+    }
 
     @Test
     public void accessDenied() throws Exception {
         this.mockMvc.perform(get("/announcements"))
                 .andDo(print())
-                .andExpect(status().is3xxRedirection())
+                .andExpect(status().isFound())
                 .andExpect(redirectedUrl("http://localhost/login"));
 
     }
 
     @Test
     public void badCredentials() throws Exception {
-        this.mockMvc.perform((post("/login").param("user", "Alfred")))
+        this.mockMvc.perform((post("/login").param("username", "Alfred").param("password", "test")))
                 .andDo(print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/login?error"));
     }
 
     @Test
     public void correctLogin() throws Exception {
         this.mockMvc.perform(formLogin().user("admin").password("admin"))
                 .andDo(print())
-                .andExpect(status().is3xxRedirection())
+                .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/"));
 
     }
 
     @Test
     public void registration() throws Exception {
-        this.mockMvc.perform(post(("/registration")).with(csrf()).param("login", "testUser").param("password", "testPassword"))
-                .andExpect(status().is2xxSuccessful());
+
+        String login = "testUser";
+        String password = "testPassword";
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+        this.mockMvc.perform(post(("/registration")).content(userJson)
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk());
 
 
     }
@@ -74,21 +132,28 @@ public class LoginTest {
 
         String login = "testUser";
         String password = "testPassword";
-
-        this.mockMvc.perform(post(("/registration")).param("login", login).param("password", password).with(csrf()))
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+        this.mockMvc.perform(post(("/registration")).content(userJson)
+        .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().isOk());
 
 
-
-        this.mockMvc.perform(post("/login").with(csrf()).param("username", login).param("password", password))
+        this.mockMvc.perform(post("/login").param("username", login).param("password", password))
                 .andDo(print())
-                .andExpect(status().is3xxRedirection())
+                .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/"));
 
         this.mockMvc.perform(get("/announcements").with(user(login)))
                 .andDo(print())
-                .andExpect(content().string(containsString("announcement_1")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].description",is("announcement_1")))
+                .andExpect(jsonPath("$.totalElements",is(1)));
+
 
 
     }
