45877abe3f9aef2c2f13093e383ce13feedb9309
Pavel Mastyaev <psmas.pm@gmail.com>
2019-08-16 17:39:04 +0400

    deleted test

--- frontend/src/src/app/sign-in/credentials.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CredentialsModel } from './credentials.model';
-
-describe('Credentials.Model', () => {
-  it('should create an instance', () => {
-    expect(new CredentialsModel()).toBeTruthy();
-  });
-});
